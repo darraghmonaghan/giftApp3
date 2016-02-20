c789567d5519425d3ceb37293a021b6d17e1bec3
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-02-20 23:32:29 +0000

    some views, with group creation underway

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,35 @@
 Rails.application.routes.draw do
 
-  get 'votes/new'
-
-  get 'votes/create'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'invites/new'
-
-  get 'invites/create'
-
-  get 'groups/index'
-
-  get 'groups/show'
-
-  get 'groups/new'
-
-  get 'groups/create'
-
-  get 'gifts/new'
-
-  get 'gifts/create'
-
   devise_for :users
 
   resources :dashboard
 
   root to: 'home#index'
+
+  get '/votes/new', to: 'votes#new', as: 'votes_new'
+
+  post '/votes/create', to: 'votes#create', as: 'votes_create'
+
+  get '/posts/new', to: 'posts#new', as: 'posts_new'
+
+  post '/posts/create', to: 'posts#create', as: 'posts_create'
+
+  get '/invites/new'
+
+  get '/invites/create'
+
+  get '/groups/index'
+
+  get '/groups/show/:id', to: 'groups#show', as: 'groups_show'
+
+  get '/groups/new', to: 'groups#new', as: 'groups_new'
+
+  post '/groups/create', to: 'groups#create', as: 'create_group'
+
+  get '/gifts/new'
+
+  get '/gifts/create'
+
+
 
 end
