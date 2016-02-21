3435beefda0624c86890e3c03f67d363948d4d68
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-02-21 12:45:23 +0000

    updated styling and data rendering

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,5 +3,14 @@ class VotesController < ApplicationController
   end
 
   def create
+  	@vote = Vote.create(vote_params)
+  	redirect_to :back
   end
+
+
+private
+  def vote_params
+    params.require(:vote).permit(:gift_id, :user_id)  
+  end
+
 end
