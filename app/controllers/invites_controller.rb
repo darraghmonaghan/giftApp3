a87e0c42ce2659b4d61c23f2ac7e8cff5ecb7832
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-02-26 18:09:27 +0000

    working invitation, with controller filter empty submissions

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -9,26 +9,37 @@ class InvitesController < ApplicationController
   	invite_addresses = []
   	group_id = params[:invite][:group_id]
 
-  	invite_addresses.push(params[:invite][:email1])
-  	invite_addresses.push(params[:invite][:email2])
-  	invite_addresses.push(params[:invite][:email3])
-  	invite_addresses.push(params[:invite][:email4])
-  	invite_addresses.push(params[:invite][:email5])
+  	if params[:invite][:email1] != '' 
+        invite_addresses.push(params[:invite][:email1])
+  	end
 
+    if params[:invite][:email2] != '' 
+        invite_addresses.push(params[:invite][:email2])
+  	end
 
-  	puts 'list of params here in array?'
-  	puts invite_addresses
+    if params[:invite][:email3] != '' 
+        invite_addresses.push(params[:invite][:email3])
+  	end
+
+    if params[:invite][:email4] != '' 
+        invite_addresses.push(params[:invite][:email4])
+  	end
+
+    if params[:invite][:email5] != '' 
+        invite_addresses.push(params[:invite][:email5])
+    end
+
 
   	invite_addresses.each do | email |
 
-			invite = Invite.new(email: email, group_id: @group_id) # Make a new Invite
-			invite.sender_id = current_user.id # set the sender to the current user
+			invite = Invite.new(email: email, group_id: group_id)            # Make a new Invite
+			invite.sender_id = current_user.id                               # set the sender to the current user
 			   
 			   if invite.save
 			  
-			   	  	if invite.recipient != nil									# If the person inviting is already signed up
-				   	  	InviteMailer.invite_existing_user(invite).deliver_now	# Version 1 of email sent
-						invite.recipient.groups.push(invite.group)			# Updating the membership list of the existing user - add the group in question
+			   	  	if invite.recipient != nil									             # If the person inviting is already signed up
+				   	  	InviteMailer.invite_existing_user(invite).deliver_now	 # Version 1 of email sent
+						invite.recipient.groups.push(invite.group)			           # Updating the membership list of the existing user - add the group in question
 
 				  	else
 					    InviteMailer.group_invite(invite, new_user_registration_path(:invite_token => invite.token)).deliver_now #send the invite data to our mailer to deliver the email
