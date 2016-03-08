8e1c1ed5f181e01a3f8e7af7b9aad855a6bab01d
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-08 23:37:43 +0000

    registrations controller - creating membership

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,22 +11,24 @@ class Users::RegistrationsController < Devise::RegistrationsController
 	def create
 		super do
 			@newUser = User.create(resource_params)
-			@newUser.save
+			puts 'user object here?'
+			puts @newUser
 			@token = params[:invite_token]
 
 					  if @token != nil
-					     @group = Invite.find_by_token(@token).group 	#find the organization attached to the invite
-					     @newUser.groups.push(@group) 					#add this user to the new organization as a member
-					     Membership.create(:user_id => @newUser.id, :group_id => @group.id)
+					     group = Invite.find_by_token(@token).group 	# find the organization attached to the invite
+					     @newUser.groups.push(group) 					# add this user to the new organization as a member
+					     sign_in @newUser
+					     Membership.create(:user_id => current_user.id, :group_id => @group.id)
 					  end
-			end
+		end
 	end
 
 
 
 	private :resource_params
 		def resource_params
-		    params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+		    params.require(:user).permit(:email, :firstname, :surname, :password, :password_confirmation, :current_password)
 		end
 
 
