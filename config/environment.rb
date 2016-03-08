d83701d46bec7c874c90c2d0a80e76a86401e077
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-08 11:28:03 +0000

    setting up sendgrid mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+config.action_mailer.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['app48004472@heroku.com'],
+  :password       => ENV['4iuwdetq9443'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
