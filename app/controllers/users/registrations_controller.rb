b652870a483a97341f3e06dcd7c7798292a8196c
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-08 17:20:58 +0000

    signup page rendering with customized devise controller - new

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,17 +1,24 @@
-Users::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
 
-def new
-	@token = params[:invite_token]
-end
 
-def create
-  @newUser = build_user(user_params)
-  @newUser.save
-  @token = params[:invite_token]
-  if @token != nil
-     group =  Invite.find_by_token(@token).group #find the organization attached to the invite
-     @newUser.groups.push(group) #add this user to the new organization as a member
-  else
-    # do normal registration things #
-  end
+	def new
+		super do
+			@token = params[:invite_token]
+		end
+	end
+
+
+	def create
+	  @newUser = build_user(user_params)
+	  @newUser.save
+	  @token = params[:invite_token]
+
+			  if @token != nil
+			     group =  Invite.find_by_token(@token).group #find the organization attached to the invite
+			     @newUser.groups.push(group) #add this user to the new organization as a member
+			  else
+			    # do normal registration things #
+			  end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => 'omniauth_callbacks'}
-
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks', registrations: 'users/registrations'}
 
   resources :dashboard
 
