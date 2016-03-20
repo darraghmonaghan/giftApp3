8e7ca0e6b47f2ee94d4e59faa2baeca2cdc6de51
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-20 20:47:04 +0000

    update to stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -10,7 +10,8 @@
 if Rails.env.production?
   Rails.configuration.stripe = {
     publishable_key: ENV[ 'STRIPE_PUBLISHABLE_KEY' ],
-    secret_key:      ENV[ 'STRIPE_SECRET_KEY' ]
+    secret_key:      ENV[ 'STRIPE_SECRET_KEY' ],
+    client_id: ENV[ 'CLIENT_ID' ]
   }
 else
   Rails.configuration.stripe = {
