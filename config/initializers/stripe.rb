64fefc668935c0dabd504c61ff4c865ce45ed7da
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-20 14:37:47 +0000

    live keys added for production environment

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,22 @@
- 
 
-Rails.configuration.stripe = {
-      :publishable_key => Rails.application.secrets.publishable_key,
-      :secret_key      => Rails.application.secrets.secret_key
-}
+# Rails.configuration.stripe = {
+#   :publishable_key => ENV['stripe_publishable_key'],
+#   :secret_key      => ENV['stripe_secret_key']
+# }
+
+
+
+
+if Rails.env.production?
+  Rails.configuration.stripe = {
+    publishable_key: ENV[ 'STRIPE_PUBLISHABLE_KEY' ],
+    secret_key:      ENV[ 'STRIPE_SECRET_KEY' ]
+  }
+else
+  Rails.configuration.stripe = {
+    publishable_key: 'pk_test_7Xrz5QNBAV1vJCf3hRMBEtKS', #Both test keys are fakes in case you're wondering
+    secret_key:      'sk_test_vGMPaW1vKvvvu5KFZzazu4rQ'
+  }
+end
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
