1e141c19faab77d9e2d909361afff31414895220
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2016-03-24 18:07:38 +0000

    updated styling on homepage and group tweaks

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20160320112920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "contacts", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "gifts", force: :cascade do |t|
     t.string   "description"
     t.string   "URL"
